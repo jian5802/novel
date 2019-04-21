017de7eceb07f82e0fd50336e28c471f54e64c38
jian5802 <839067094@qq.com>
2019-04-21 19:32:18 +0800

    init backstage

--- backstage/src/main.js
+++ backstage/src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from './common/api/http'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/base.scss'
+import qs from 'qs'
+
+// 设置请求数据格式(转换成字符串型formData)
+axios.defaults.transformRequest = data => {
+  return qs.stringify(data)
+}
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
